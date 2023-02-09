db447c7513bf8e5bf1edb1d71392adacdcf36b15
Arseniy <71733778+J3olchara@users.noreply.github.com>
2023-02-09 16:14:08 +0300

    delete uselles key generation

--- get_key.py
+++
@@ -1,6 +0,0 @@
-from django.core.management.utils import get_random_secret_key
-
-
-if __name__ == '__main__':
-    with open('.env', 'w') as env:
-        env.write(F'SECRET_KEY="{get_random_secret_key()}"')

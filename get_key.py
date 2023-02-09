1e56cbd961af14cce7451042cee4483d0ed450dd
Arseniy <senya.borodulin@mail.ru>
2023-02-09 14:32:59 +0300

    update readme.md

--- get_key.py
+++ get_key.py
@@ -3,4 +3,4 @@ from django.core.management.utils import get_random_secret_key
 
 if __name__ == '__main__':
     with open('.env', 'w') as env:
-        env.write(F'SECRET_KEY=\'{get_random_secret_key()}\'')
+        env.write(F'SECRET_KEY="{get_random_secret_key()}"')

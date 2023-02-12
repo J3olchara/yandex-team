b8c6875209d78116273cb9267a0505fc179f5d40
Arseniy <senya.borodulin@mail.ru>
2023-02-13 00:34:56 +0300

    fix quotes

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -5,102 +5,102 @@ from typing import Any, Dict, List, Optional, Union
 
 from dotenv import load_dotenv
 
-if not load_dotenv(Path(r"..\.env")):
-    load_dotenv(Path(r".env"))
+if not load_dotenv(Path(r'..\.env')):
+    load_dotenv(Path(r'.env'))
 
 BASE_DIR: Path = Path(__file__).resolve().parent.parent
 
-SECRET_KEY: str = os.getenv("SECRET_KEY", "not_secret_key")
+SECRET_KEY: str = os.getenv('SECRET_KEY', 'not_secret_key')
 
-DEBUG: Optional[bool] = os.getenv("DJANGO_DEBUG", "False").lower() in (
-    "true",
-    "1",
-    "t",
+DEBUG: Optional[bool] = os.getenv('DJANGO_DEBUG', 'False').lower() in (
+    'true',
+    '1',
+    't',
 )
 
-ALLOWED_HOSTS: List[str] = str(os.getenv("DJANGO_HOSTS", "*")).split()
+ALLOWED_HOSTS: List[str] = str(os.getenv('DJANGO_HOSTS', '*')).split()
 
 INSTALLED_APPS: List[str] = [
-    "about.apps.AboutConfig",
-    "catalog.apps.CatalogConfig",
-    "homepage.apps.HomepageConfig",
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "debug_toolbar",
+    'about.apps.AboutConfig',
+    'catalog.apps.CatalogConfig',
+    'homepage.apps.HomepageConfig',
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE: List[str] = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "debug_toolbar.middleware.DebugToolbarMiddleware",
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 INTERNAL_IPS = [
-    "127.0.0.1",
+    '127.0.0.1',
 ]
 
-ROOT_URLCONF: str = "lyceum.urls"
+ROOT_URLCONF: str = 'lyceum.urls'
 
 TEMPLATES: List[Dict[str, Any]] = [
     {
-        "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
-        "APP_DIRS": True,
-        "OPTIONS": {
-            "context_processors": [
-                "django.template.context_processors.debug",
-                "django.template.context_processors.request",
-                "django.contrib.auth.context_processors.auth",
-                "django.contrib.messages.context_processors.messages",
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
             ],
         },
     },
 ]
 
-WSGI_APPLICATION: str = "lyceum.wsgi.application"
+WSGI_APPLICATION: str = 'lyceum.wsgi.application'
 
 DATABASES: Dict[str, Dict[str, Union[str, Path]]] = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
 AUTH_PASSWORD_VALIDATORS: List[Dict[str, str]] = [
     {
-        "NAME": "django.contrib.auth."
-        + "password_validation."
-        + "UserAttributeSimilarityValidator",
+        'NAME': 'django.contrib.auth.'
+        + 'password_validation.'
+        + 'UserAttributeSimilarityValidator',
     },
     {
-        "NAME": "django.contrib.auth."
-        + "password_validation."
-        + "MinimumLengthValidator",
+        'NAME': 'django.contrib.auth.'
+        + 'password_validation.'
+        + 'MinimumLengthValidator',
     },
     {
-        "NAME": "django.contrib.auth."
-        + "password_validation."
-        + "CommonPasswordValidator",
+        'NAME': 'django.contrib.auth.'
+        + 'password_validation.'
+        + 'CommonPasswordValidator',
     },
     {
-        "NAME": "django.contrib.auth."
-        + "password_validation."
-        + "NumericPasswordValidator",
+        'NAME': 'django.contrib.auth.'
+        + 'password_validation.'
+        + 'NumericPasswordValidator',
     },
 ]
 
-LANGUAGE_CODE: str = "ru-ru"
+LANGUAGE_CODE: str = 'ru-ru'
 
-TIME_ZONE: str = "Europe/Moscow"
+TIME_ZONE: str = 'Europe/Moscow'
 
 USE_I18N: bool = True
 
@@ -108,6 +108,6 @@ USE_L10N: bool = True
 
 USE_TZ: bool = True
 
-STATIC_URL: str = "/static/"
+STATIC_URL: str = '/static/'
 
-DEFAULT_AUTO_FIELD: str = "django.db.models.BigAutoField"
+DEFAULT_AUTO_FIELD: str = 'django.db.models.BigAutoField'

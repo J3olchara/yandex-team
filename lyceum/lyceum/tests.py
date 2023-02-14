187ec4cc083fdbc85829538d8ee12dd0ef434d19
Arseniy <senya.borodulin@mail.ru>
2023-02-14 19:44:28 +0300

    added CoffeeTime middleware tests'

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,6 +1,6 @@
 """MIDDLEWARE tests"""
+import mock
 from django.test import RequestFactory, TestCase
-from mock import MagicMock
 
 from . import middlewares
 
@@ -14,7 +14,7 @@ class ReverseMiddlewareTests(TestCase):
 
     def test_middleware_reverser(self) -> None:
         """test reversing middleware work"""
-        get_response = MagicMock()
+        get_response = mock.MagicMock()
         request = self.factory.get('/')
         middleware = middlewares.CoffeeTime(get_response)
         response = middleware(request)

da058d6be84c8222eb09c8eea22e8dd4391fb6fc
Arseniy <senya.borodulin@mail.ru>
2023-02-28 23:59:31 +0300

    added static/media files and templates

--- lyceum/lyceum/urls.py
+++ lyceum/lyceum/urls.py
@@ -1,22 +1,21 @@
 """lyceum URL Configuration"""
-from typing import List, Union
+from typing import List, Union, Any
 
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path, resolvers
 
-urlpatterns: Union[
-    List[resolvers.URLResolver],
-    List[Union[resolvers.URLResolver, resolvers.URLPattern]],
-    List[object],
-] = [
+urlpatterns: Any = [
     path('admin/', admin.site.urls, name='admin'),
     path('', include('homepage.urls')),
     path('catalog/', include('catalog.urls')),
     path('about/', include('about.urls')),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+urlpatterns += static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
 
 
 if settings.DEBUG:

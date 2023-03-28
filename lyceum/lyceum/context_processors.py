a7b3046e5d7ce633ca516777b21296a6e45e2f83
Arseniy <senya.borodulin@mail.ru>
2023-03-29 00:00:55 +0300

    test: added tests to birthday context processors

--- lyceum/lyceum/context_processors.py
+++ lyceum/lyceum/context_processors.py
@@ -3,15 +3,16 @@ Context processor
 
 creates some variables for all templates
 """
-from datetime import date
 from typing import Any, Dict
 
 import authorisation.models as auth_models
 from django.http import HttpRequest
+from django.utils.timezone import datetime
 
 
 def today_birthday_processor(request: HttpRequest) -> Dict[str, Any]:
-    today = date.today()
+    today = datetime.today()
+    print(today)
     return {
         'today_birthdays': auth_models.Profile.objects.filter(
             birthday__day=today.day, birthday__month=today.month

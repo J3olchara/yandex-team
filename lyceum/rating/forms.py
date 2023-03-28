9b10185602149ae54d43c386be9e78143cdce755
Mattew-Kentor <popok3580@gmail.com>
2023-03-28 23:06:23 +0300

    finished

--- lyceum/rating/forms.py
+++ lyceum/rating/forms.py
@@ -7,11 +7,15 @@ class EvaluationForm(forms.ModelForm):
     class Meta:
         model = models.Evaluation
         fields = ('value',)
-        widgets = {'value': forms.NumberInput(attrs={
-            'type': 'range',
-            'class': 'form-range mb-5',
-            'step': '1',
-            'min': '0',
-            'max': '5',
-            'value': '5',
-        })}
+        widgets = {
+            'value': forms.NumberInput(
+                attrs={
+                    'type': 'range',
+                    'class': 'form-range mb-5',
+                    'step': '1',
+                    'min': '0',
+                    'max': '5',
+                    'value': '5',
+                }
+            )
+        }

42c1cb9cab7ef4914971e99c11f24d7985b7aef8
Arseniy <senya.borodulin@mail.ru>
2023-03-30 00:36:00 +0300

    fix: UserDetail view, deleted uselless fixtures and created database visual diagramm

--- lyceum/users/views.py
+++ lyceum/users/views.py
@@ -3,7 +3,6 @@ from typing import Any, Dict
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.forms.models import model_to_dict
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
 from django.views import generic
 
